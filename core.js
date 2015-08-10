cb4184d64ca4998b863409fcbba4897d35e779f2
Jules Moretti <julesmoretti@me.com>
2015-08-09 21:45:58 -0700

    bug fix

--- core.js
+++ core.js
@@ -1097,6 +1097,8 @@ var crypto                                = require('crypto'),
         var user_updates = req.query.user_updates;
         var user_id = req.query.user_id;
 
+        console.log( 'user_mysql_updates', JSON.stringify( user_mysql_updates ) );
+
         connection.query('UPDATE access_right SET '+user_mysql_updates+' WHERE token = "'+userToken+'"; INSERT INTO addition SET category = "new_users", category_id = '+user_id , function( err, rows, fields ) {
           if (err) throw err;
 
