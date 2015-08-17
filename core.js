52772a8484c949f2dbdff18815d723befa287486
Jules Moretti <julesmoretti@me.com>
2015-08-17 02:40:06 -0700

    added console logs

--- core.js
+++ core.js
@@ -1113,6 +1113,10 @@ var crypto                                = require('crypto'),
         var user_updates = req.query.user_updates;
         var user_id = req.query.user_id;
 
+        console.log("user_mysql_updates: ", user_mysql_updates);
+        console.log("user_mysql_updates - stringifyed: ", JSON.stringify( user_mysql_updates ) );
+
+
         connection.query('UPDATE access_right SET '+user_mysql_updates+' WHERE token = "'+userToken+'"; INSERT INTO addition SET category = "new_users", category_id = '+user_id , function( err, rows, fields ) {
           if (err) throw err;
 
